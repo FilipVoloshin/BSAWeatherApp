4f7e4d3ba6fc05248e00cd4091e56bb0731f6bfd
A09F\filip <filippvoloshin@gmail.com>
2017-07-10 01:05:44 +0300

    added unobtrusive jQuery to the project

--- BSAWeatherApp/App_Start/BundleConfig.cs
+++ BSAWeatherApp/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace BSAWeatherApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
