e715afa9ec5b05d33542806a1972dc924d50fe66
A09F\filip <filippvoloshin@gmail.com>
2017-07-10 00:58:30 +0300

    fixed bootstrap, jquery, and fontawesome

---
+++ BSAWeatherApp/Scripts/weatherForecast.js
@@ -0,0 +1,14 @@
+﻿function changeSelectType() {
+    var cityDropdownVal = $("#defaultCity").val();
+    if (cityDropdownVal !== "") {
+        $("#customCity").attr('disabled', true);
+        $("#customCity").val("");
+    }
+    else {
+        $("#customCity").attr('disabled', false);
+    }
+}
+
+$("#defaultCity").change(function () {
+    changeSelectType();
+});
