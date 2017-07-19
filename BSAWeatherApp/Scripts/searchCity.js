1cfc6c24c13e21069e4ca62f714b14fb30a5317f
A09F\filip <filippvoloshin@gmail.com>
2017-07-20 00:30:48 +0300

    updated weather Home view look

--- BSAWeatherApp/Scripts/searchCity.js
+++ BSAWeatherApp/Scripts/searchCity.js
@@ -1,9 +1,11 @@
 ﻿$(function () {
     $("#addClass").click(function () {
         $('#qnimate').addClass('popup-box-on');
+        $('.header-bar, .body-content').wrap('<div class="blur-all">');
     });
 
     $("#removeClass").click(function () {
         $('#qnimate').removeClass('popup-box-on');
+        $('.blur-all').removeClass();
     });
 })
