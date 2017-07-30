d36fa31b2e1902e72958aa069a90bed527c16e71
devreactor_f <lajdakua@gmail.com>
2017-07-30 21:55:01 +0300

    deployed UWP application

---
+++ BSAWeather.UWP/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSAWeather.UWP
+{
+    public class ViewModelLocator
+    {
+    }
+}
